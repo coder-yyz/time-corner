6471667bd5800cc566637898f2050fc4e2ba8125
yangyongzhi <yangyongzhi@eastmoney.com>
2025-03-21 14:47:19 +0800

    生成配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+const routerConfig = require('./router.config.json')
+
+module.exports = {
+  title: 'Time Corner',
+  description: '前端学习笔记',
+  base: '/',
+  theme: 'reco',
+  // head: [
+  //   [
+  //     'script',
+  //     {},
+  //     `var _hmt = _hmt || [];
+  //     (function() {
+  //       var hm = document.createElement("script");
+  //       hm.src = "https://hm.baidu.com/hm.js?2f7c6e1628f708d263ab46ce7fac7777";
+  //       var s = document.getElementsByTagName("script")[0];
+  //       s.parentNode.insertBefore(hm, s);
+  //     })();
+  //     `,
+  //   ],
+  // ],
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+  },
+  extraWatchFiles: [
+    '.vuepress/router.config.json',
+  ],
+  themeConfig: {
+    subSidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '关于我',
+        items: [
+          { text: 'Github', link: 'https://github.com/coder-yyz' },
+        ],
+      },
+    ],
+    sidebar: routerConfig,
+    sidebarDepth: 4,
+    displayAllHeaders: true,
+  },
+}
---
+++ init.js
@@ -0,0 +1,64 @@
+// 生成配置文件
+const path = require('path')
+const fs = require('fs')
+
+const docs_path = path.resolve(__dirname, './docs')
+
+console.log('🚀🚀🚀 开始生成配置文件 🚀🚀', docs_path)
+
+// 递归遍历文件夹生成 router 配置
+const router = []
+function gen_router_config(dir_path, ref, parent_router_path) {
+  try {
+    const file_or_dir_list = fs.readdirSync(dir_path)
+    file_or_dir_list.forEach((file_or_dir_name) => {
+      const current_path = path.join(dir_path, file_or_dir_name)
+      const current_stat = fs.statSync(current_path)
+      // .vuepress 文件夹 或者 assets 文件夹  或者 images 文件夹
+      if (/^\./.test(file_or_dir_name) || file_or_dir_name === 'assets' || file_or_dir_name === 'images') {
+        return
+      }
+      // 文件
+      if (current_stat.isFile()) {
+        const current_router_path = parent_router_path
+          ? `${parent_router_path}${file_or_dir_name}`
+          : `/${file_or_dir_name}`
+        if (/^\README/.test(file_or_dir_name)) {
+          return
+        }
+        const file = {
+          title: file_or_dir_name,
+          path: current_router_path
+        }
+        ref.push(file)
+        return
+      }
+      // 文件夹
+      if (current_stat.isDirectory()) {
+        const current_router_path = parent_router_path
+          ? `${parent_router_path}${file_or_dir_name}/`
+          : `/${file_or_dir_name}/`
+        // 创建分组
+        const group = {
+          title: file_or_dir_name,
+          path: current_router_path,
+          children: []
+        }
+        ref.push(group)
+        gen_router_config(current_path, ref[ref.length-1].children, current_router_path)
+      }
+    })
+  } catch (error) {
+    console.error('💣 ERROR:: gen_router_config error',error)
+  }
+}
+gen_router_config(docs_path, router, '')
+router.unshift({ title: '主页', path: '/' })
+const json = JSON.stringify(router)
+fs.writeFile('./docs/.vuepress/router.config.json', json, (err) => {
+  if (err){
+    console.log('💣 ERROR:: node fs.writeFile', err)
+    throw err
+  }
+  console.log('🚀🚀🚀 router 配置已生成 🚀🚀')
+})
